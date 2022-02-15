1cf107b7c09633554d88e9307fa39d04f0c54145
EC2 Default User <ec2-user@ip-172-31-4-87.ec2.internal>
2022-02-15 12:42:33 +0000

    first_commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,44 @@
 class BooksController < ApplicationController
-  def new
+
+
+
+  def create
+    book = Book.new(book_params)
+
+    book.save
+
+    # redirect_to '/books/index'
   end
 
   def index
+    @books = Book.all#Bookのtitle,bodyが
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])#ここの構成復習！！！！！！
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])#ルーティング→コントローラ→ビュー
+    book.destroy
+    redirect_to books_path#移動先のルーティングPrefixを参照。urlも
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get '/top' => "homes#top"
-  get '/books/edit' => "books#edit"
-  get '/books/index' => "books#index"
-  get '/books/new' => "books#new"
-  get '/books/show' => "books#show"
-  get '/books' => "books#index"
+  # post '/books/new' => "books#new"
+  # get '/books/edit' => "books#edit"
+  # get '/books/index' => "books#index"
+  # get '/books/new' => "books#new"
+  # get '/books/show' => "books#show"
+  # get '/books' => "books#index"
+  resources :books
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
